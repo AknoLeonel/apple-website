ff38c97d4def5799f40c2ab62488e7717915de3f
Akno Leonel <gamer.aknologia@gmail.com>
2026-01-15 07:01:23 -0300

    salvo

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,12 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    sentryVitePlugin({
-      org: "jsm-x9", // Replace with your actual Sentry organization slug
-      project: "javascript-react", // Replace with your actual Sentry project name
-      include: "./dist", // Directory to scan for sourcemaps
-      authToken: process.env.SENTRY_AUTH_TOKEN, // Use environment variables for sensitive data
-      setCommits: {
-        auto: true // Automatically associate commits with releases
-      }
-    })
-  ],
-  base: "/apple-website/",
+  plugins: [react()],
+  // CORREÇÃO: Removemos o 'base: "/apple-website/"'
+  // Agora o site vai funcionar na raiz (vercel.app)
   build: {
-    sourcemap: true
+    sourcemap: false // Otimização: deixa o site mais leve
   }
 });
